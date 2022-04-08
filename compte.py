8d8a70df0ca6b1ecadaddab1858d7bc8ea87491c
RoiBann <manuchauveau@outlook.fr>
2022-04-08 16:36:25 +0200

    Premier commit

--- compte.py
+++ compte.py
@@ -4,3 +4,5 @@ class Compte:
         self._numero_compte = numero_compte
         self._nom_proprietaire = nom_proprietaire
         self._solde = solde
+    def play(self):
+        print('coucou')
