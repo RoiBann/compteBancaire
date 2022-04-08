d733466db1a39b16068ccdc6d4a0730714b16a43
RoiBann <manuchauveau@outlook.fr>
2022-04-08 16:36:25 +0200

    Premier commit

---
+++ compte.py
@@ -0,0 +1,6 @@
+class Compte:
+
+    def __init__(self, numero_compte, nom_proprietaire, solde):
+        self._numero_compte = numero_compte
+        self._nom_proprietaire = nom_proprietaire
+        self._solde = solde
